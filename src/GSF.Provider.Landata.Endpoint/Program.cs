b22392b147e8449a386174d15ff8002d3d479102
Drew Freyling <drew.freyling@globalx.com.au>
2019-05-31 16:10:22 +1000

    get build working
    add a c# file
    
    first commit

---
+++ src/GSF.Provider.Landata.Endpoint/Program.cs
@@ -0,0 +1,18 @@
+// A Hello World! program in C#.
+
+using System;
+
+namespace GSF.Provider.Landata.Endpoint
+{
+    class Program 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
